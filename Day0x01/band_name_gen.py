c07522f6d0b6b78c749172328934b205b4aa59b9
Yosef-S-A <yosefsamuel22@gmail.com>
2021-08-24 11:03:24 +0000

    a python script that generates a band name based on user input

---
+++ Day0x01/band_name_gen.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+# Greeting
+print("Welcome to the Band Name Generator.")
+city = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's the name of your first pet?\n")
+
+print("Your band name could be " + city + " " + pet_name)
