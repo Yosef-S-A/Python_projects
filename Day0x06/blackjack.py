82b78a6473c7f32c758fa52e10eef8fda89f4ecf
Yosef-S-A <yosefsamuel22@gmail.com>
2021-09-02 09:15:32 +0000

    blackjack debugging

--- Day0x06/blackjack.py
+++ Day0x06/blackjack.py
@@ -6,6 +6,7 @@ import random
 
 print(logo)
 
+# list and dictionary important for the game
 card_deck = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 card_value = {
     'A': [1, 11],
@@ -25,6 +26,7 @@ card_value = {
 
 
 def play():
+    '''A function that intiates game play'''
     choice = input(
         "Do you want to play a game of Blackjack? Type 'y' or 'n': ")
     if choice == 'y':
@@ -37,9 +39,10 @@ def play():
         computer_list = []
         player_score = 0
         computer_score = 0
-
-        player_list = [random.choice(card_deck), random.choice(card_deck)]
-        computer_list = [random.choice(card_deck), random.choice(card_deck)]
+	
+        # initial card distibution
+        player_list = [hit(), hit()]
+        computer_list = [hit(), hit()]
 
         player_score = get_score(player_list, player_score)
 
@@ -52,12 +55,14 @@ def play():
 
 
 def game_play():
+    ''' A function that controls the game play'''
     global player_list
     global computer_list
     global player_score
     global computer_score
 
-    print("Your cards: {}, current score: {}".format(player_list, player_score))
+    print("Your cards: {}, current score: {}".format(
+        player_list, player_score))
 
     print("Computer's first card: {}".format(computer_list[0]))
 
@@ -77,24 +82,29 @@ def game_play():
                 computer_score = get_score(computer_list, computer_score)
             if computer_score <= 21:
                 if computer_score > player_score:
-		 print("Your final hand: {}, final score: {}".format(player_list, player_score))
-		 print("Computer's final hand: {}, final score: {}".format(
-		     computer_list, computer_score))
-		 print("You Lose")
-            	elif computer_score == player_score:
-		 print("Your final hand: {}, final score: {}".format(player_list, player_score))
-		 print("Computer's final hand: {}, final score: {}".format(
-		     computer_list, computer_score))
-		 print("Push \n It's a tie.")
-		else:
-		 print("Your final hand: {}, final score: {}".format(player_list, player_score))
-		 print("Computer's final hand: {}, final score: {}".format(
-		     computer_list, computer_score))
-		 print("You win!!!")
-	   else:
-		print("Your final hand: {}, final score: {}".format(player_list, player_score))
-		print("Computer's final hand: {}, final score: {}".format(computer_list, computer_score))
-		print("You win!!!")
+                    print("Your final hand: {}, final score: {}".format(
+                        player_list, player_score))
+                    print("Computer's final hand: {}, final score: {}".format(
+                        computer_list, computer_score))
+                    print("You Lose")
+                elif computer_score == player_score:
+                    print("Your final hand: {}, final score: {}".format(
+                        player_list, player_score))
+                    print("Computer's final hand: {}, final score: {}".format(
+                        computer_list, computer_score))
+                    print("Push \n It's a tie.")
+                else:
+                    print("Your final hand: {}, final score: {}".format(
+                        player_list, player_score))
+                    print("Computer's final hand: {}, final score: {}".format(
+                        computer_list, computer_score))
+                    print("You win!!!")
+            else:
+                print("Your final hand: {}, final score: {}".format(
+                    player_list, player_score))
+                print("Computer's final hand: {}, final score: {}".format(
+                    computer_list, computer_score))
+                print("You win!!!")
 
         elif player_choice == 'y':
             player_list.append(hit())
@@ -106,12 +116,14 @@ def game_play():
 
 
 def hit():
+    '''A function that controls card distribution'''
     return random.choice(card_deck)
 
 
 def get_score(sent_list, score):
     '''send score of cards'''
     if sent_list.__contains__('A'):
+        score = 0
         for x in sent_list:
             if x == 'A' and (score + 11) < 21:
                 score += card_value[x][1]
@@ -121,6 +133,7 @@ def get_score(sent_list, score):
                 score += card_value[x]
 
     else:
+        score = 0
         for x in sent_list:
             score += card_value[x]
     return score
