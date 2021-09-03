b701d2d692f33f6a2a50c4f5c5e401012d94dd59
Yosef-S-A <yosefsamuel22@gmail.com>
2021-09-03 09:57:51 +0000

    number guessing game

---
+++ Day0x07/num_guess.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+import random
+from art import logo
+
+rand_num = random.randrange(1, 101)
+
+EASY_LEVEL_TURNS = 10
+HARD_LEVEL_TURNS = 5
+
+def compare():
+	global lives
+	while lives != 0:
+		print ("You have {} attempts remaining to guess the number".format(lives))
+		guess = int(input("Make a guess: "))
+		if (rand_num > guess):
+			print("Too low")
+		elif (rand_num < guess):
+			print("Too high")
+		else: 
+			print("You got it! The answer was {}".format(rand_num))
+			break
+		lives -= 1
+	else:
+		print ("You've run out of guesses, you lose.")
+
+def choose_level():
+	global lives
+	level_choice = input("Choose a difficulty. Type 'easy' or 'hard': ")
+	if level_choice == 'easy':
+		lives = EASY_LEVEL_TURNS
+	else:
+		lives = HARD_LEVEL_TURNS
+
+def play():
+	print("Psst, the correct anwer is {}".format(rand_num))
+	choose_level()
+	compare()
+
+print (logo)
+
+print ("Welcome to the Number Guessing Gmae!!")
+
+print ("I'm thinking of a number between 1 and 100.")
+
+play() 
