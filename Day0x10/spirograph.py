56d1c93bebf24823da3588963b7b3a8e745accef
Yosef-S-A <yosefsamuel22@gmail.com>
2021-09-10 21:25:31 +0000

    spirograph update

--- Day0x10/spirograph.py
+++ Day0x10/spirograph.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
 
 import turtle as turtle_module
-import random
+from random import randint
 
+turtle_module.colormode(255)
+
+tim = turtle_module.Turtle()
+screen = turtle_module.Screen()
+
+
+def random_color():
+    r = randint(0, 255)
+    g = randint(0, 255)
+    b = randint(0, 255)
+    return r, g, b
 
 
 def draw_spirograph(size_of_gap):
@@ -13,4 +24,8 @@ def draw_spirograph(size_of_gap):
         tim.setheading(current_heading + size_of_gap)
         tim.circle(150)
 
+
 draw_spirograph(2)
+
+screen.exitonclick()
+
